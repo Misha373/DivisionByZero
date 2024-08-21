7ddbbb502f4dfe544cf09e31d49fe2694b1728ae
Instamaikle <misasapka1@gmail.com>
2024-08-21 22:42:11 +0300

    Калькулятор створено

--- DivisionByZero/Program.cs
+++ DivisionByZero/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Програма калькулятор");
+
+Console.WriteLine("Введіть число a:");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть число b:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть операццію між числами a і b:");
+
+char operation = Convert.ToChar(Console.ReadLine());
+
+try
+{
+    int v = 0;
+
+    int g = 10 / v;
+    if (operation == '/')
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {a / b}");
+    }
+    else if (operation == '*')
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {a * b}");
+    }
+    else if (operation == '-')
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {a - b}");
+    }
+    else if (operation == '+')
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {a + b}");
+    }
+    else if (operation == '^')
+    {
+        Console.WriteLine($"Рішенням математичної задачі є: {Math.Pow(a, b)}");
+    }
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Ділення на нуль забороенно");
+}
+catch (Exception)
+{
+    Console.WriteLine("Вираз не має змісту");
+}
+
